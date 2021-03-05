a38ac39f8b78b25c101111a8cd1ca7bccfa9cb5a
Alexandru Fiodor <alexandrufiodor95@gmail.com>
2021-03-05 10:54:30 +0200

    01. tests

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {splitIntoWords} from "./01-hello-tests/01";
 
+splitIntoWords('Hello my friends!');
+console.log(splitIntoWords('Hello my friend!'));
 ReactDOM.render(
   <React.StrictMode>
     <App />
